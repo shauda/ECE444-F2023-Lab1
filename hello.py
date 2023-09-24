a5b2ce6cd938058a72907709227b71e7e124ae1d
root <root@Shauda.localdomain>
2023-09-24 03:34:31 -0400

    Modified example in chapter 4 of Flask Web Development Textbook to meet lab 1 requirements.

--- hello.py
+++ hello.py
@@ -3,11 +3,12 @@ from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from datetime import datetime
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SubmitField, EmailField
 from wtforms.validators import DataRequired
 
 class NameForm(FlaskForm):
     name = StringField('What is your name?', validators=[DataRequired()])
+    email = EmailField('What is your UofT Email address?', validators=[DataRequired()])
     submit = SubmitField('Submit')
 
 app = Flask(__name__)
@@ -48,11 +49,22 @@ def index():
     form = NameForm()
     if form.validate_on_submit():
         old_name = session.get('name')
+        old_email = session.get('email')
+        
         if old_name is not None and old_name != form.name.data:
             flash('Looks like you have changed your name!')
+        if old_email is not None and old_email != form.email.data:
+            flash('Looks like you have changed your email!')
+        
         session['name'] = form.name.data
+
+        if 'utoronto' in form.email.data:
+            session['email'] = form.email.data
+        else:
+            session['email'] = 'BAD'
+
         return redirect(url_for('index'))
-    return render_template('index.html', form=form, name=session.get('name'))
+    return render_template('index.html', form=form, name=session.get('name'), email=session.get('email'))
 
 # @app.route('/user/<name>')
 # def user(name):
