a5e7e99fc882c62a0220a4fccbd32dbac9f82766
root <root@Shauda.localdomain>
2023-09-24 02:49:48 -0400

    Reproduced examples from chapter 4 upto ex 4-7 from Flask Web Development Textbook

--- hello.py
+++ hello.py
@@ -1,9 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from datetime import datetime
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+
+class NameForm(FlaskForm):
+    name = StringField('What is your name?', validators=[DataRequired()])
+    submit = SubmitField('Submit')
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hard to guess string'
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
@@ -30,10 +38,22 @@ def internal_server_error(e):
 # def index():
 #     return render_template('index.html')
 
-@app.route('/') 
-def index(): 
-    return render_template('index.html', current_time=datetime.utcnow())
+# ex 3-7
+# @app.route('/') 
+# def index(): 
+#     return render_template('index.html', current_time=datetime.utcnow())
 
-@app.route('/user/<name>')
-def user(name):
-    return render_template('user.html', name=name)
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    form = NameForm()
+    if form.validate_on_submit():
+        old_name = session.get('name')
+        if old_name is not None and old_name != form.name.data:
+            flash('Looks like you have changed your name!')
+        session['name'] = form.name.data
+        return redirect(url_for('index'))
+    return render_template('index.html', form=form, name=session.get('name'))
+
+# @app.route('/user/<name>')
+# def user(name):
+#     return render_template('user.html', name=name)
